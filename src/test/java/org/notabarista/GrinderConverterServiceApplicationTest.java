0dced692be2152c636f9c3002aa1993e77c57bfe
Tudor Codrea <codrea_tudor@yahoo.com>
2021-12-14 09:46:06 +0200

    fixes build

--- src/test/java/org/notabarista/GrinderConverterServiceApplicationTest.java
+++ src/test/java/org/notabarista/GrinderConverterServiceApplicationTest.java
@@ -3,10 +3,12 @@ package org.notabarista;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class GrinderConverterServiceApplicationTest {
 
 	@Test
